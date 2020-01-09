e8f82a56d4e87b8a6e9fe425661a7a53986c6839
sl1738521409 <1738521409@qq.com>
2020-01-09 14:43:22 +0800

    Initial commit

---
+++ src/main/java/com/aaa/gongjijin/GongjijinApplication.java
@@ -0,0 +1,13 @@
+package com.aaa.gongjijin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GongjijinApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GongjijinApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/aaa/gongjijin/GongjijinApplicationTests.java
@@ -0,0 +1,13 @@
+package com.aaa.gongjijin;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GongjijinApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
